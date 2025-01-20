9561ec1af6ea17f76018741566127787af9695df
als8921 <als8921@naver.com>
2025-01-20 14:15:23 +0900

    CH01  refactor : 함수 실행시 계좌 정보 입력

--- Ch01/AccountManager.cpp
+++ Ch01/AccountManager.cpp
@@ -32,11 +32,13 @@ struct Account
 map<int, Account> AccountList;
 vector<int> AccountIDList;
 
+void ShowUserInfo();
 void ShowUserInfo(int id);
 void ShowAllUserInfo();
-void CreateAccount(string name);
-void Deposit(int id, int money);
-void WithDraw(int id, int money);
+void CreateAccount();
+void Deposit();
+void WithDraw();
+bool IsExistAccount(int id);
 
 int main()
 {
@@ -53,20 +55,21 @@ int main()
         cout << "======================================================\n";
 
         int command;
+        cout << "선택 : ";
         cin >> command;
         switch (command)
         {
         case CREATE:
-            CreateAccount("Aaaa");
+            CreateAccount();
             break;
         case DEPOSIT:
-            Deposit(0, 10000);
+            Deposit();
             break;
         case WITHDRAW:
-            WithDraw(0, 6000);
+            WithDraw();
             break;
         case SHOWINFO:
-            ShowUserInfo(0);
+            ShowUserInfo();
             break;
         case SHOWALLINFO:
             ShowAllUserInfo();
@@ -79,8 +82,11 @@ int main()
     }
 }
 
-void CreateAccount(string name)
+void CreateAccount()
 {
+    string name;
+    cout << "[계좌 생성]\n";
+    cout << "이름 입력해주세요 : "; cin >> name;
     int id = AccountIDList.size();
     AccountIDList.push_back(id);
 
@@ -89,9 +95,28 @@ void CreateAccount(string name)
     newAccount.AccountID = id;
 
     AccountList[id] = newAccount;
+
+    cout << "고객의 이름        : " << AccountList[id].Name << "\n";
+    cout << "고객의 계좌 번호   : " << AccountList[id].AccountID << "\n";
+    cout << "계좌 잔액          : " << AccountList[id].Money << "\n";
+    cout << "======================================================\n";
+}
+void ShowUserInfo()
+{
+    int id;
+    cout << "[계좌 조회]\n";
+    cout << "계좌번호를 입력해주세요 : "; cin >> id;
+    if(!IsExistAccount(id)) return;
+
+    cout << "고객의 이름        : " << AccountList[id].Name << "\n";
+    cout << "고객의 계좌 번호   : " << AccountList[id].AccountID << "\n";
+    cout << "계좌 잔액          : " << AccountList[id].Money << "\n";
+    cout << "======================================================\n";
 }
 void ShowUserInfo(int id)
 {
+    cout << "[계좌 조회]\n";
+
     cout << "고객의 이름        : " << AccountList[id].Name << "\n";
     cout << "고객의 계좌 번호   : " << AccountList[id].AccountID << "\n";
     cout << "계좌 잔액          : " << AccountList[id].Money << "\n";
@@ -103,23 +128,44 @@ void ShowAllUserInfo()
         for (int id : AccountIDList)
             ShowUserInfo(id);
 }
-void Deposit(int id, int money)
+void Deposit()
 {
+    int id, money;
+    cout << "[입금]\n";
+    cout << "계좌번호를 입력해주세요 : "; cin >> id;
+    cout << "입금할 금액을 입력해주세요 : "; cin >> money;
     AccountList[id].Money += money;
+
+
+    cout << money << "원 입금이 완료되었습니다.\n";
+    cout << "잔액 : " << AccountList[id].Money << "원\n";
+    cout << "======================================================\n";
 }
-void WithDraw(int id, int money)
+void WithDraw()
 {
+    int id, money;
+    cout << "[출금]\n";
+    cout << "계좌번호를 입력해주세요 : "; cin >> id;
+    cout << "출금할 금액을 입력해주세요 : "; cin >> money;
     if(AccountList[id].Money >= money)
     {
         AccountList[id].Money -= money;
         cout << money << "원 출금이 완료되었습니다.\n";
-        cout << "잔액 : " << AccountList[id].Money << "원\n";
-        cout << "======================================================\n";
     }
     else
     {   
         cout << "잔액이 부족합니다.\n";
-        cout << "잔액 : " << AccountList[id].Money << "원\n";
-        cout << "======================================================\n";
     }
+    cout << "잔액 : " << AccountList[id].Money << "원\n";
+    cout << "======================================================\n";
+}
+
+bool IsExistAccount(int id)
+{
+    for(int _id : AccountIDList)
+        if(_id == id) return true;
+
+    
+    cout << "존재하지 않는 계좌번호 입니다.\n";
+    return false;
 }
